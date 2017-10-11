ae182f1891ac53e3ca55db54c8d7a55dc8aa7a2f
Deniss <den.b@itservicesgroup.co.uk>
2017-10-11 17:35:31 +0100

    Add Campaigns controller, :index, :show

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,12 @@
 class CampaignsController < ApplicationController
+
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def show
+    id = params[:id]
+    @users = Campaign.find_by(id: id).users
+  end
+
 end
--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe CampaignsController, type: :controller do
-
+  describe '#index' do
+    it 'should have success message' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe '#show' do
+    it 'should have list of users' do
+      campaign = Campaign.create(name: "campaign")
+      %w(Leon Anthony Lilit).each do |name|
+        campaign.users.create(name: name, count: 12)
+      end
+      get :show, params: {id: campaign.id}
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
